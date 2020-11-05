2a4123962aed50a4090aed26e924552e1c29f234
moritzmn <muellernavarra@gmail.com>
2020-11-05 17:44:40 +0100

    Introducing FindOmega. Func is marked as deprecated. Plot Function removed

--- R/UtilityFunctions.R
+++ R/UtilityFunctions.R
@@ -1,11 +1,13 @@
 #' Returns predictor vector for design matrix
-#' @description Returns predictor vector for design matrix from 44 astronomical angular velocities.
+#' @description Returns predictor vector for design matrix from 39 astronomical angular velocities.
 #' @param xi Transit index
 #' @param tdiff Length of input time series.
 #' @return A list with the selected angular velocities, their ranks and the predictor vector (Values between -1, 1).
 #' @export  
 
 Funcs <-  function(tdiff, xi) {
+  
+  .Deprecated(package = "TideTables", msg = "This function is deprecated and will be removed in later releases.")
   
     #Vector with angular velocities
     rad <- 0.017453292519943
@@ -127,7 +129,153 @@ Funcs <-  function(tdiff, xi) {
     }
     
     return(list(omega_sel, omega_sel_rank, afunc))
+}
+
+#' Returns omegas and their ranks.
+#' @description Returns omegas and their ranks from 39 astronomical angular velocities.
+#' @param tdiff Length of input time series.
+#' @return A list with the selected angular velocities and their ranks.
+#' @export 
+
+FindOmega <- function(tdiff) {
+  #Vector with angular velocities
+  #rad <- 0.017453292519943
+  #xi  <- rad * xi
+  
+  omegas <- vector()
+  omegas[ 1]<-0.0548098
+  omegas[ 2]<-0.2306165
+  omegas[ 3]<-1.0201944
+  omegas[ 4]<-1.8097724
+  omegas[ 5]<-2.0403886
+  omegas[ 6]<-11.5978420
+  omegas[ 7]<-11.7131503
+  omegas[ 8]<-12.3874200
+  omegas[ 9]<-12.6180365
+  omegas[10]<-12.7881545
+  omegas[11]<-13.5229227
+  omegas[12]<-13.6382309
+  omegas[13]<-13.6930407
+  omegas[14]<-13.7535391
+  omegas[15]<-15.9640460
+  omegas[16]<-24.2158785
+  omegas[17]<-25.1812631
+  omegas[18]<-25.2360729
+  omegas[19]<-26.2562673
+  omegas[20]<-27.0458453
+  omegas[21]<-27.1611535
+  omegas[22]<-27.2764618
+  omegas[23]<-27.3312716
+  omegas[24]<-36.9492232
+  omegas[25]<-38.7589956
+  omegas[26]<-38.8743038
+  omegas[27]<-38.9896120
+  omegas[28]<-40.7993844
+  omegas[29]<-49.4519514
+  omegas[30]<-50.4721458
+  omegas[31]<-52.5125347
+  omegas[32]<-52.5673444
+  omegas[33]<-54.5529235
+  omegas[34]<-62.1852961
+  omegas[35]<-63.9950685
+  omegas[36]<-64.2256849
+  omegas[37]<-75.7082187
+  omegas[38]<-77.7486076
+  omegas[39]<-100.9442917
+  
+  omega_pos <- omegas
+  
+  iranks <- vector()
+  iranks[ 1]<-6
+  iranks[ 2]<-13
+  iranks[ 3]<-7
+  iranks[ 4]<-31
+  iranks[ 5]<-17
+  iranks[ 6]<-14
+  iranks[ 7]<-8
+  iranks[ 8]<-34
+  iranks[ 9]<-19
+  iranks[10]<-39
+  iranks[11]<-3
+  iranks[12]<-4
+  iranks[13]<-38
+  iranks[14]<-21
+  iranks[15]<-36
+  iranks[16]<-11
+  iranks[17]<-35
+  iranks[18]<-1
+  iranks[19]<-12
+  iranks[20]<-33
+  iranks[21]<-15
+  iranks[22]<-2
+  iranks[23]<-27
+  iranks[24]<-10
+  iranks[25]<-16
+  iranks[26]<-24
+  iranks[27]<-22
+  iranks[28]<-23
+  iranks[29]<-29
+  iranks[30]<-5
+  iranks[31]<-9
+  iranks[32]<-37
+  iranks[33]<-30
+  iranks[34]<-25
+  iranks[35]<-28
+  iranks[36]<-26
+  iranks[37]<-20
+  iranks[38]<-18
+  iranks[39]<-32
+  
+  omega_pos_rank <- iranks
+  omega_sel      <- vector()
+  omega_sel_rank <- vector()
+  
+  while (length(omega_pos) > 0) {
+    #Find most relevant velocities
+    maxrank <- which.min(omega_pos_rank)
+    omega_pos_maxrank <- omega_pos[maxrank]
+    if( omega_pos_maxrank * tdiff > 360){
+      omega_sel       <- c(omega_sel,  omega_pos_maxrank)
+      omega_sel_rank  <- c(omega_sel_rank, omega_pos_rank[maxrank])
+      del_index       <- which((abs(omega_pos -  omega_pos_maxrank) * tdiff) < 360)
+      omega_pos       <- omega_pos[-del_index]
+      omega_pos_rank  <- omega_pos_rank[-del_index]
+    } else {
+      omega_pos      <- omega_pos[-maxrank]
+      omega_pos_rank <- omega_pos_rank[-maxrank]
+    }
   }
+  omega_sel <- omega_sel[order(omega_sel)]
+  
+  return(list(omega_sel, omega_sel_rank))
+  
+}
+
+#' #' Returns predictor vector for design matrix
+#' @description Returns predictor vector for design matrix from 39 astronomical angular velocities.
+#' @param xi Transit index
+#' @param omega The return value of FindOmega().
+#' @return A list with the selected angular velocities, their ranks and the predictor vector (Values between -1, 1).
+#' @export 
+
+ComputeAfunc <- function(omega = NULL, xi = NULL) {
+  
+  rad <- 0.017453292519943
+  xi  <- rad * xi
+  omega_sel <- omega[[1]]
+  o_length  <- length(omega_sel)
+  #Computing the afuncs for the selected omegas
+  afunc <- vector(mode = "numeric", length = 2 * o_length + 1)
+  afunc[1] <- 1.00000
+  
+  for(i in seq.int(2, 2 * o_length + 1, 2)) {
+    oxi          <- omega_sel[i / 2] * xi
+    afunc[i]     <- cos(oxi)
+    afunc[i + 1] <- sin(oxi)
+  }
+  omega[[3]] <- afunc
+  return(omega)
+}
 
 #' Calculates numm and k4
 #' @description Calculates transit number (numm) and high (1, 3) or low (2, 4) water number (k4).
@@ -156,83 +304,6 @@ NumCulm <- function(t, tmhwi){
   nummk4$k4       <- as.numeric(1 + (floor(tm2 * 2 ) %% 4))
   
   return(nummk4)
-}
-
-#' Plot function
-#' @description Plots the computed tides
-#' @param data Output from the TideTables function
-#' @return Generates eight plots
-#' @importFrom graphics plot
-#' @importFrom graphics par
-#' @export 
-
-PlotTides <- function(data){
-  
-  .Deprecated(package = "TideTables", msg = "This function is deprecated and will be removed in later releases. Please make up your own plot.")
-  
-  synthesis <- data$c.table
-  
-  high_or_low_water      <- NULL
-  upper_or_lower_transit <- NULL
-  i                      <- NULL
-  height                 <- NULL
-  st.transit             <- NULL
-  numm                   <- NULL
-  date_time              <- NULL
-  V1                     <- NULL
-  prediction_date        <- NULL
-  prediction_time        <- NULL
-  
-  par(mfrow = c(4, 2))
-  #Height
-  plot(x    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 1), i],
-       y    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 1), height],
-       type = "l",
-       xlab = "i",
-       ylab = "Height",
-       main = "High Water - Upper Transit")
-  plot(x    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 1), i],
-       y    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 1), height],
-       type = "l",
-       xlab = "i",
-       ylab = "Height",
-       main = "Low Water - Upper Transit")
-  plot(x    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 0), i],
-       y    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 0), height],
-       type = "l",
-       xlab = "i",
-       ylab = "Height",
-       main = "High Water - Lower Transit")
-  plot(x    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 0), i],
-       y    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 0), height],
-       type = "l",
-       xlab = "i",
-       ylab = "Height",
-       main ="Low Water - Lower Transit")
-  #Time
-  plot(x    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 1), i],
-       y    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 1), st.transit],
-       type = "l",
-       xlab = "i",
-       ylab = "Time",
-       main = "High Water - Upper Transit")
-  plot(x    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 1), i],
-       y    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 1), st.transit],
-       type = "l",
-       xlab = "i",
-       ylab = "Time",
-       main = "Low Water - Upper Transit")
-  plot(x    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 0), i],
-       y    = synthesis[(high_or_low_water == 1 & upper_or_lower_transit == 0), st.transit],type="l",
-       xlab = "i",
-       ylab = "Time",
-       main = "High Water - Lower Transit")
-  plot(x    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 0), i],
-       y    = synthesis[(high_or_low_water == 0 & upper_or_lower_transit == 0), st.transit],
-       type = "l",
-       xlab = "i",
-       ylab = "Time",
-       main = "Low Water - Lower Transit")
 }
 
 #' Calculates tmhwi
