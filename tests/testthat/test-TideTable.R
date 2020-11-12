ee531c0e3dfc010c8bf4e6c25add4e54c658c2ad
moritzmn <muellernavarra@gmail.com>
2020-11-12 12:17:39 +0100

    comparing c.table with SynTT return. saving results in objects for better style

--- tests/testthat/test-TideTable.R
+++ tests/testthat/test-TideTable.R
@@ -1,16 +1,16 @@
-context("Equal return")
+context("Equal return TideTable, SynTT")
 library(TideTables)
 
+tt_tt <- TideTable(dataInput=observation, asdate=observation$observation_date[1], 
+          astime=observation$observation_time[1], 
+          aedate="1991/05/01", aetime="21:00:00", ssdate="1995/01/01", 
+          sstime="00:00:00", sedate="1995/01/31", setime="21:00:00")[["c.table"]]
+
+tt_syn <- SynTT(tmodel = BuildTT(dataInput = observation, asdate = observation$observation_date[1],
+                                 astime = observation$observation_time[1],
+                                 aedate = "1991/05/01", aetime = "21:00:00"),
+                ssdate = "1995/01/01", sstime = "00:00:00", sedate = "1995/01/31", setime = "21:00:00")
 
 test_that("equal return values of TideTable vs BuildTT+SynTT", {
-  expect_equal(TideTable(dataInput=observation, asdate=observation$observation_date[1], 
-                         astime=observation$observation_time[1], 
-                         aedate="1991/05/01", aetime="21:00:00", ssdate="1995/01/01", 
-                         sstime="00:00:00", sedate="1995/01/31", setime="21:00:00"), 
-               SynTT(tmodel = BuildTT(dataInput = observation, asdate = observation$observation_date[1],
-                                      astime = observation$observation_time[1],
-                                      aedate = "1991/05/01", aetime = "21:00:00"),
-                     ssdate = "1995/01/01", sstime = "00:00:00", sedate = "1995/01/31", setime = "21:00:00"
-                     )
-  )
+  expect_equal(tt_tt, tt_syn)
 })
